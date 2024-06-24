5b9537a6f728457c09c10035dcfb915b68138d28
Diego Taquiri <diego.taquiri@upch.pe>
2024-06-24 10:27:38 -0500

    first commit

---
+++ static/main.js
@@ -0,0 +1,32 @@
+import {loadGLTF} from "./libs/loader.js";
+import {mockWithVideo} from './libs/camera-mock.js';
+const THREE = window.MINDAR.IMAGE.THREE;
+
+document.addEventListener('DOMContentLoaded', () => {
+  const start = async() => {
+    //mockWithVideo('../../assets/mock-videos/musicband1.mp4');
+
+    const mindarThree = new window.MINDAR.IMAGE.MindARThree({
+      container: document.body,
+      imageTargetSrc: '/static/assets/targets/targets-eminem.mind',
+    });
+    const {renderer, scene, camera} = mindarThree;
+
+    const light = new THREE.HemisphereLight( 0xffffff, 0xbbbbff, 1 );
+    scene.add(light);
+
+    //const raccoon = await loadGLTF('/static/assets/models/upch-dia/upch.gltf');
+    const raccoon = await loadGLTF('/static/assets/models/spartan_armour_mkv_-_halo_reach/scene.gltf');
+    raccoon.scene.scale.set(0.1, 0.1, 0.1);
+    raccoon.scene.position.set(0, -0.4, 0);
+
+    const anchor = mindarThree.addAnchor(0);
+    anchor.group.add(raccoon.scene);
+
+    await mindarThree.start();
+    renderer.setAnimationLoop(() => {
+      renderer.render(scene, camera);
+    });
+  }
+  start();
+});
